a21e932a4878b3c39bd7d260f744f5860ac53761
Vu Nguyen <Vu Nguyen>
2016-08-09 20:45:11 +0700

    ts v1.1 seo

---
+++ app/config/auth.js
@@ -0,0 +1,34 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '105660646519031', // your App ID
+        'clientSecret'  : '5033b154b10d7720e948294345583ed2', // your App Secret
+        'callbackURL'   : 'http://localhost:8000/auth/facebook/callback', 
+        'profileFields': ["id", "email", "gender", "about", "birthday", "cover"]
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '873026146341-ce0605glrtsfebtiqt0vnsmd3k9nidfi.apps.googleusercontent.com',
+        'clientSecret'  : '923j9wnJZPWdsJEzahQz0egI',
+        'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+    }, 
+
+    'mailer' : {
+        service: "Gmail",
+        port: 465,
+        user: 'nguyencongvu@gmail.com',
+        pass: 'Go.1b.16', 
+        from: 'system@domain.com', //-- reply To   
+        templates: "../public/templates",
+    }
+
+};
---
+++ app/config/database.js
@@ -0,0 +1,22 @@
+// -- local     
+var dbName = 'socialCRM'; //--EDIT in config 
+// var connectionString = 'mongodb://localhost:27017/' + dbName;
+var connectionString = 'mongodb://127.0.0.1:27017/' + dbName;
+
+//-- Openshift 
+if (process.env.OPENSHIFT_MONGODB_DB_URL){
+    dbName = "teamshow";
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_URL + dbName;
+    //connectionString = "mongodb://admin:wn9bHy6xpnPu@$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/"+ dbName;
+}
+
+//-- mLab mongodb -- khong dung!
+var mLab = true; 
+if (mLab==true) { 
+    dbName = 'web01'; //--EDIT in config 
+    connectionString = 'mongodb://admin:admin@ds023694.mlab.com:23694/'+ dbName;
+} 
+
+module.exports = {
+    'connectionString': connectionString
+}; 
