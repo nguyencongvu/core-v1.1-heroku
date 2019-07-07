ab361b969558c5d80647de1bba342999f37bf71c
Nguyen Cong Vu <nguyencongvu@gmail.com>
2019-07-08 03:25:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').createServer(),
+    io     = require('socket.io')(server);
 
-app.get('/', function(req, res) {
-   res.sendfile('index.html');
+
+io.on('connection', function(socket){
+
+  var room = socket.handshake['query']['r_var'];
+
+  socket.join(room);
+  console.log('user joined room #'+room);
+
+  socket.on('disconnect', function() {
+    socket.leave(room)
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    io.to(room).emit('chat message', msg);
+  });
+
 });
 
-var roomno = 1;
-io.on('connection', function(socket) {
-   
-   //Increase roomno 2 clients are present in a room.
-   if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
-   socket.join("room-"+roomno);
-
-   //Send this event to everyone in the room.
-   io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
-})
-
-http.listen(3000, function() {
-   console.log('listening on localhost:3000');
-});
+server.listen(3000);
